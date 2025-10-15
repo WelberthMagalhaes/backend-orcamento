adb0423c8f9abca6e38a0660df47f75518c6daa7
Welberth Araujo <welberth.araujo@acessorias.com>
2025-10-15 02:01:37 -0300

    fix: Adiciona url do front no .env

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   // Configurar CORS
   app.enableCors({
-    origin: 'http://localhost:3001', // porta do Next.js
+    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
     credentials: true,
   });
 
