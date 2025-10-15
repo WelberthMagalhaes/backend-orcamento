5d94c8f7247e05e95a2b6d3d9b094196b5fb64a6
Welberth Araujo <welberth.araujo@acessorias.com>
2025-10-15 01:39:29 -0300

    config: Configura CORS permitindo requisicao por outro endereco.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Configurar CORS
+  app.enableCors({
+    origin: 'http://localhost:3001', // porta do Next.js
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 3000).catch(console.error);
 }
-bootstrap();
+bootstrap().catch(console.error);
